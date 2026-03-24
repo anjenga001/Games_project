924e42d667fc8ec15d8e69d79938fd2b01a55513
Austin <schoolausto@gmail.com>
2026-03-24 02:13:46 -0400

    cleaned all the data and exported to csv file

--- game_scripts/transform.py
+++ game_scripts/transform.py
@@ -8,9 +8,15 @@ df = pd.json_normalize(data)
 
 df = df.drop(columns=['screenshots', 'micro_trailer', 'gameplay', 'short_description', 'image', 'link'])
 
+df = df[['id', 'name', 'year', 'rating.mean', 'rating.count']]
+
 df['year'] = df['year'].astype(int)
 df['rating.count'] = df['rating.count'].astype('Int64')
 df['rating.mean'] = df['rating.mean'].round(2)
 
-df.dropna()
-print(df.head())
+df = df.dropna()
+
+df = df.drop_duplicates(subset='id', keep='first')
+
+
+df.to_csv('games_last_year.csv', index=False)
